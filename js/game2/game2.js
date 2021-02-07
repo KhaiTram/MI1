361ec1d6debd7b6d0cc255be527633aca10ce998
Khai <k-h.t@hotmail.com>
2021-02-07 02:10:15 +0100

    minor changes

--- js/game2/game2.js
+++ js/game2/game2.js
@@ -2,73 +2,156 @@
 
 const Game = function () {
 
-    // Erstellung der Gamewelt
-    this.world = {
+    this.world = new Game.World();
 
-
-        background_color:"rgba(40,48,56,0.25)",
-
-        //Physikattribute
-        friction: 0.9,
-        gravity: 3,
-
-        //Initialisierung eines Spielers
-        player: new Game.Player(),
-
-        player: new Game.Player(),
-        
-        height: 1080,
-        width: 1920,
-
-        //Kollisionskontrolle: Die Position des  übermittelten Objektes wird auf Kollision mit der Spelewelt überprüft 
-        collideObject: function (object) {
-
-            // Position des Objektes wird auf den linken Rand der Welt zurückgesetzt sobald er diesen überschreitet
-            if (object.x < 0) {
-                object.x = 0; 
-                object.velocity_x = 0;
-            }
-
-            //Position des Objektes wird mit seiner Breite addiert damit man die Position der rechten Seite des Objektes bekommt
-            else if (object.x + object.width > this.width) {
-                object.x = this.width - object.width; object.velocity_x = 0;
-            }
-
-            //Selbes spiel auf vertikaler Ebene Hier kann er nicht durch die decke
-            if (object.y < 0) {
-                object.y = 0; object.velocity_y = 0;
-            }
-
-            //wenn der Spieler auf dem Boden ist wird sein jumping attribut auf false gesetzt damit er springen kann
-            else if (object.y + object.height > this.height) {
-                object.jumping = false;
-                object.y = this.height - object.height; object.velocity_y = 0;
-            }
-
-        },
-
-        // Spielerpositionen werden upgedated mit Kollisionskontrolle und berücksichtigung der Spielephysik
-        update: function () {
-
-
-            this.player.velocity_y += this.gravity;
-            this.player.update();
-
-            this.player.velocity_x *= this.friction;
-            this.player.velocity_y *= this.friction;
-
-            this.collideObject(this.player);
-
-        }
-
-    };
-
-    
     this.update = function () {
 
         this.world.update();
 
     };
+
+};
+
+Game.World = function (friction = 0.8, gravity = 2) {
+
+    this.collider = new Game.World.Collider();
+
+    this.friction = friction;
+    this.gravity = gravity;
+
+
+
+    this.spriteSheet = new Game.World.SpriteSheet(8, 16);
+    this.player = new Game.World.Object.Player(100, 100);
+
+
+    this.height = 1080;
+    this.width = 1920;
+
+};
+
+Game.prototype = {
+
+    constructor: Game,
+
+};
+
+Game.World.prototype = {
+
+    constructor: Game.World,
+  
+    collideObject:function(object) {
+  
+      if      (object.getLeft()   < 0          ) { object.setLeft(0);             object.velocity_x = 0; }
+      else if (object.getRight()  > this.width ) { object.setRight(this.width);   object.velocity_x = 0; }
+      if      (object.getTop()    < 0          ) { object.setTop(0);              object.velocity_y = 0; }
+      else if (object.getBottom() > this.height) { object.setBottom(this.height); object.velocity_y = 0; object.jumping = false; }
+  
+      var bottom, left, right, top, value;
+  
+      top    = Math.floor(object.getTop()    / this.tile_set.tile_size);
+      left   = Math.floor(object.getLeft()   / this.tile_set.tile_size);
+      value  = this.collision_map[top * this.columns + left];
+      this.collider.collide(value, object, left * this.tile_set.tile_size, top * this.tile_set.tile_size, this.tile_set.tile_size);
+  
+      top    = Math.floor(object.getTop()    / this.tile_set.tile_size);
+      right  = Math.floor(object.getRight()  / this.tile_set.tile_size);
+      value  = this.collision_map[top * this.columns + right];
+      this.collider.collide(value, object, right * this.tile_set.tile_size, top * this.tile_set.tile_size, this.tile_set.tile_size);
+  
+      bottom = Math.floor(object.getBottom() / this.tile_set.tile_size);
+      left   = Math.floor(object.getLeft()   / this.tile_set.tile_size);
+      value  = this.collision_map[bottom * this.columns + left];
+      this.collider.collide(value, object, left * this.tile_set.tile_size, bottom * this.tile_set.tile_size, this.tile_set.tile_size);
+  
+      bottom = Math.floor(object.getBottom() / this.tile_set.tile_size);
+      right  = Math.floor(object.getRight()  / this.tile_set.tile_size);
+      value  = this.collision_map[bottom * this.columns + right];
+      this.collider.collide(value, object, right * this.tile_set.tile_size, bottom * this.tile_set.tile_size, this.tile_set.tile_size);
+  
+    },
+  
+    /* This function changed to update the player's position and then do collision,
+    and then update the animation based on the player's final condition. */
+    update:function() {
+  
+      this.player.updatePosition(this.gravity, this.friction);
+  
+      this.collideObject(this.player);
+  
+      this.player.updateAnimation();
+  
+    }
+  
+  };
+
+// Erstellung der Gamewelt
+this.world = {
+
+
+    background_color: "black",
+
+    //Physikattribute
+    friction: 0.9,
+    gravity: 3,
+
+    //Initialisierung eines Spielers
+    player: new Game.Player(),
+
+    player: new Game.Player(),
+
+    height: 1080,
+    width: 1920,
+
+    //Kollisionskontrolle: Die Position des  übermittelten Objektes wird auf Kollision mit der Spelewelt überprüft 
+    collideObject: function (object) {
+
+        // Position des Objektes wird auf den linken Rand der Welt zurückgesetzt sobald er diesen überschreitet
+        if (object.x < 0) {
+            object.x = 0;
+            object.velocity_x = 0;
+        }
+
+        //Position des Objektes wird mit seiner Breite addiert damit man die Position der rechten Seite des Objektes bekommt
+        else if (object.x + object.width > this.width) {
+            object.x = this.width - object.width; object.velocity_x = 0;
+        }
+
+        //Selbes spiel auf vertikaler Ebene Hier kann er nicht durch die decke
+        if (object.y < 0) {
+            object.y = 0; object.velocity_y = 0;
+        }
+
+        //wenn der Spieler auf dem Boden ist wird sein jumping attribut auf false gesetzt damit er springen kann
+        else if (object.y + object.height > this.height) {
+            object.jumping = false;
+            object.y = this.height - object.height; object.velocity_y = 0;
+        }
+
+    },
+
+    // Spielerpositionen werden upgedated mit Kollisionskontrolle und berücksichtigung der Spielephysik
+    update: function () {
+
+
+        this.player.velocity_y += this.gravity;
+        this.player.update();
+
+        this.player.velocity_x *= this.friction;
+        this.player.velocity_y *= this.friction;
+
+        this.collideObject(this.player);
+
+    }
+
+};
+
+
+this.update = function () {
+
+    this.world.update();
+
+};
 
 };
 
--- js/game2/main2.js
+++ js/game2/main2.js
@@ -2,69 +2,102 @@
 
 /*Jede Funktion die Elemente aus 2 Funktionen nutzt sollte in der Main.js definiert werden.
 Wir wollen sowenig zwischen den einzelnen Komponenten referenzierehn wie möglich
-*/ 
-window.addEventListener("load", function(event) {
+*/
+window.addEventListener("load", function (event) {
 
-    "use strict";
-  
-    
+  "use strict";
+
+  /* Der Assetmanager sorgt dafür dass Grafiken in das Spiel geladen werden */
+  const AssetsManager = function () {
+
+    this.spriteSheetImage = undefined;
+
+  };
+
+  AssetsManager.prototype = {
+
+    constructor: Game.AssetsManager,
+
+    loadSpriteSheetImage: function (url, callback) {
+
+      this.spriteSheetImage = new Image();
+
+      this.spriteSheetImage.addEventListener("load", function (event) {
+
+        callback();
+
+      }, { once: true });
+
+      this.spriteSheetImage.src = url;
+
+    }
+
+  };
+
+  /* Folgend auf Inputevents könnten Änderungen auf dem Display erfolgen, 
+  deshalb werden Input Event Listener in der Main Methode behalten 
+  Diese Funtkion überprüft das Event und verarbeitet es wenn es sich um eine Tasteneingabe handelt
+  (Siehe KeyDownUp in der Controller Klasse)*/
+  var keyDownUp = function (event) {
+
+    controller.keyDownUp(event.type, event.keyCode);
+
+  };
+
+  /* Diese Funktion passt die Größe des Spielefensters an die Größe des browserfensters an mit nem geringen offset von 32 pixel*/
+  var resize = function (event) {
+    display.resize(document.documentElement.clientWidth - 32, document.documentElement.clientHeight - 32, game.world.height / game.world.width);
+    display.render();
+  };
+
+  /* Zeichnet unsere Spielewelt neu und nutzt dabei Funktionen aus der Display Klasse. mehr details siehe display klasse */
+  var render = function () {
+
+    let frame = game.world.spriteSheet.frames[game.world.player.frame_value];
+
+    display.drawObject(assets_manager.spriteSheetImage,
+    frame.x, frame.y,
+    game.world.player.x + Math.floor(game.world.player.width * 0.5 - frame.width * 0.5) + frame.offset_x,
+    game.world.player.y + frame.offset_y, frame.width, frame.height);
+
+    display.render();
+
+  };
 
 
-    /* Folgend auf Inputevents könnten Änderungen auf dem Display erfolgen, 
-    deshalb werden Input Event Listener in der Main Methode behalten 
-    Diese Funtkion überprüft das Event und verarbeitet es wenn es sich um eine Tasteneingabe handelt
-    (Siehe KeyDownUp in der Controller Klasse)*/
-    var keyDownUp = function(event) {
-  
-      controller.keyDownUp(event.type, event.keyCode);
-  
-    };
-  
-    /* Diese Funktion passt die Größe des Spielefensters an die Größe des browserfensters an mit nem geringen offset von 32 pixel*/
-    var resize = function(event) {
-      display.resize(document.documentElement.clientWidth - 32, document.documentElement.clientHeight - 32, game.world.height / game.world.width);
-      display.render();
-    };
-  
-    /* Zeichnet unsere Spielewelt neu mer details siehe display klasse */
-    var render = function() {
-  
-      display.fill(game.world.background_color);
-      display.drawRectangle(game.world.player.x, game.world.player.y, game.world.player.width, game.world.player.height, game.world.player.color);
-      display.render();
-  
-    };
-  
+  /* Updated information in unserer Spielewelt die meist Abhängig von eingaben ist */
+  var update = function () {
+    if (controller.left.active) { game.world.player.moveLeft(); }
+    if (controller.right.active) { game.world.player.moveRight(); }
+    if (controller.up.active) { game.world.player.jump(); controller.up.active = false; }
 
-    /* Updated information in unserer Spielewelt die meist Abhängig von eingaben ist */
-    var update = function() {
-      if (controller.left.active)  { game.world.player.moveLeft();  }
-      if (controller.right.active) { game.world.player.moveRight(); }
-      if (controller.up.active)    { game.world.player.jump(); controller.up.active = false; }
-  
-      game.update();
-    };
-  
+    game.update();
+  };
+
   /* Initialisierung aller Komponenten */
-    var controller = new Controller();
-    var display    = new Display(document.querySelector("canvas"));
-    var game       = new Game();
-    var engine     = new Engine(1000/30, render, update);
-  
-    /* Das buffer canvas bekommt die gleichen Dimensionen wie die Spielewelt damit das Spiel sich der Fenstergröße anpassen kann
-    ohne diesen Schritt würde der Buffer die Größe des Fensters annehmen und das Spiel die fix definierten Werte des Game Objektes übernehmen übenehmen*/
-    display.buffer.canvas.height = game.world.height;
-    display.buffer.canvas.width = game.world.width;
-  
-    /* Eventlistener werden eingeführt um entsprechend auf Änderungen zu horchen */
-    window.addEventListener("keydown", keyDownUp);
-    window.addEventListener("keyup",   keyDownUp);
-    window.addEventListener("resize",  resize);
-  
+  var assets_manager = new AssetsManager();
+  var controller = new Controller();
+  var display = new Display(document.querySelector("canvas"));
+  var game = new Game();
+  var engine = new Engine(1000 / 30, render, update);
+
+  /* Das buffer canvas bekommt die gleichen Dimensionen wie die Spielewelt damit das Spiel sich der Fenstergröße anpassen kann
+  ohne diesen Schritt würde der Buffer die Größe des Fensters annehmen und das Spiel die fix definierten Werte des Game Objektes übernehmen übenehmen*/
+  display.buffer.canvas.height = game.world.height;
+  display.buffer.canvas.width = game.world.width;
+  display.buffer.imageSmoothingEnabled = false;
+
+
+  assets_manager.spriteSheetImage("Ninja.png", () => {
     //resize wird zum Anfang einmal ausgeführt damit das spiel sich der Größe des Fensters anpasst
     resize();
-  
     engine.start();
-  
+
   });
-  
+
+  /* Eventlistener werden eingeführt um entsprechend auf Änderungen zu horchen */
+  window.addEventListener("keydown", keyDownUp);
+  window.addEventListener("keyup", keyDownUp);
+  window.addEventListener("resize", resize);
+
+});
