d999d2374d95415ac6dc874e89bb0ddea6254867
Robert <quizzez1912@gmail.com>
2021-02-10 16:34:44 +0100

    arrow block scroll

--- js/game2/game2.js
+++ js/game2/game2.js
@@ -328,3 +328,26 @@ function sound(src) {
 }
 
 }
+
+// https://stackoverflow.com/questions/8916620/disable-arrow-key-scrolling-in-users-browser/8916697 Author: almaceleste
+
+// Deaktiviert das Scrollen auf der Website mit den Pfeiltasten 
+window.addEventListener('keydown', (e) => {
+    if (e.target.localName != 'input') {   // if you need to filter <input> elements
+        switch (e.keyCode) {
+            case 37: // left
+            case 39: // right
+                e.preventDefault();
+                break;
+            case 38: // up
+            case 40: // down
+                e.preventDefault();
+                break;
+            default:
+                break;
+        }
+    }
+}, {
+    capture: true,   // this disables arrow key scrolling in modern Chrome
+    passive: false   // this is optional, my code works without it
+});
--- js/game3/game3.js
+++ js/game3/game3.js
@@ -279,5 +279,14 @@ function sound(src) {
         this.sound.pause();
       }
 }
+function disableKeyScroll(){
+    window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.code) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
+}
 
 }
